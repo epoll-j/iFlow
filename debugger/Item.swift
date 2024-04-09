5695ad8e9cef8c5cdcf6e91899b1f46a54f999b1
Dubhe <epoll@foxmail.com>
2024-04-09 22:04:46 +0800

    Initial Commit

---
+++ debugger/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  debugger
+//
+//  Created by Dubhe on 2024/4/9.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
