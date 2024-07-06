5c60d414c388f58d11eba0548ee4e5b9b1dfdc8d
Dubhe <epoll@foxmail.com>
2024-07-06 18:13:27 +0800

    perf: format code

--- ProxyService/Utils/CertUtils.swift
+++ ProxyService/Utils/CertUtils.swift
@@ -72,72 +72,72 @@ public class CertUtils: NSObject {
     public static var certPool = NSMutableDictionary()
     
     public static func generateSelfSignedCert(host: String) -> NIOSSLCertificate {
-            let pkey = getPrivateKeyPointer(bytes: privateKey)
-            let x: OpaquePointer = CNIOBoringSSL_X509_new()!
-            CNIOBoringSSL_X509_set_version(x, 2)
-            let subject = CNIOBoringSSL_X509_NAME_new()
-            CNIOBoringSSL_X509_NAME_add_entry_by_txt(subject, "C", MBSTRING_ASC, "SE", -1, -1, 0);
-            CNIOBoringSSL_X509_NAME_add_entry_by_txt(subject, "ST", MBSTRING_ASC, "", -1, -1, 0);
-            CNIOBoringSSL_X509_NAME_add_entry_by_txt(subject, "L", MBSTRING_ASC, "", -1, -1, 0);
-            CNIOBoringSSL_X509_NAME_add_entry_by_txt(subject, "O", MBSTRING_ASC, "Tomduck", -1, -1, 0);
-            CNIOBoringSSL_X509_NAME_add_entry_by_txt(subject, "OU", MBSTRING_ASC, "", -1, -1, 0);
-            CNIOBoringSSL_X509_NAME_add_entry_by_txt(subject, "CN", MBSTRING_ASC, host, -1, -1, 0);
+        let pkey = getPrivateKeyPointer(bytes: privateKey)
+        let x: OpaquePointer = CNIOBoringSSL_X509_new()!
+        CNIOBoringSSL_X509_set_version(x, 2)
+        let subject = CNIOBoringSSL_X509_NAME_new()
+        CNIOBoringSSL_X509_NAME_add_entry_by_txt(subject, "C", MBSTRING_ASC, "SE", -1, -1, 0);
+        CNIOBoringSSL_X509_NAME_add_entry_by_txt(subject, "ST", MBSTRING_ASC, "", -1, -1, 0);
+        CNIOBoringSSL_X509_NAME_add_entry_by_txt(subject, "L", MBSTRING_ASC, "", -1, -1, 0);
+        CNIOBoringSSL_X509_NAME_add_entry_by_txt(subject, "O", MBSTRING_ASC, "Tomduck", -1, -1, 0);
+        CNIOBoringSSL_X509_NAME_add_entry_by_txt(subject, "OU", MBSTRING_ASC, "", -1, -1, 0);
+        CNIOBoringSSL_X509_NAME_add_entry_by_txt(subject, "CN", MBSTRING_ASC, host, -1, -1, 0);
 
-            var serial = randomSerialNumber()
-            CNIOBoringSSL_X509_set_serialNumber(x, &serial)
-            
-            let notBefore = CNIOBoringSSL_ASN1_TIME_new()!
-            var now = time(nil)
-            CNIOBoringSSL_ASN1_TIME_set(notBefore, now)
-            CNIOBoringSSL_X509_set_notBefore(x, notBefore)
-            CNIOBoringSSL_ASN1_TIME_free(notBefore)
-            
-            now += 60 * 60 * 60  // Give ourselves an hour
-            let notAfter = CNIOBoringSSL_ASN1_TIME_new()!
-            CNIOBoringSSL_ASN1_TIME_set(notAfter, now)
-            CNIOBoringSSL_X509_set_notAfter(x, notAfter)
-            CNIOBoringSSL_ASN1_TIME_free(notAfter)
-            
-            CNIOBoringSSL_X509_set_pubkey(x, pkey)
-            
-    //        CNIOBoringSSL_PEM_read_bio_X509(cert, nil, nil, nil)
-            
-            CNIOBoringSSL_X509_set_issuer_name(x, CNIOBoringSSL_X509_get_subject_name(_ref()!))
-            CNIOBoringSSL_X509_set_subject_name(x, subject)
-            CNIOBoringSSL_X509_NAME_free(subject)
-            addExtension(x509: x, nid: NID_basic_constraints, value: "critical,CA:FALSE")
-            addExtension(x509: x, nid: NID_subject_key_identifier, value: "hash")
-            addExtension(x509: x, nid: NID_subject_alt_name, value: "DNS:\(host)")
-            addExtension(x509: x, nid: NID_ext_key_usage, value: "serverAuth,OCSPSigning")
-            
-            CNIOBoringSSL_X509_sign(x, pkey, CNIOBoringSSL_EVP_sha256())
-            
-            // 将证书转换为 DER 格式的字节数组
-            var derBytesPointer: UnsafeMutablePointer<UInt8>? = nil
-            let derLength = CNIOBoringSSL_i2d_X509(x, &derBytesPointer)
-            
-            // 确保内存安全，创建 Data 对象，并释放分配的内存
-            let certData = Data(bytes: derBytesPointer!, count: Int(derLength))
-            CNIOBoringSSL_OPENSSL_free(derBytesPointer)
-
-            let cert = try! NIOSSLCertificate(bytes: Array(certData), format: .der)
-            CNIOBoringSSL_X509_free(x)
-            
-            return cert
-        }
+        var serial = randomSerialNumber()
+        CNIOBoringSSL_X509_set_serialNumber(x, &serial)
         
-        private static func _ref() -> OpaquePointer? {
-            let ref = cert.withUnsafeBytes { (ptr) -> OpaquePointer? in
-                let bio = CNIOBoringSSL_BIO_new_mem_buf(ptr.baseAddress, ptr.count)!
+        let notBefore = CNIOBoringSSL_ASN1_TIME_new()!
+        var now = time(nil)
+        CNIOBoringSSL_ASN1_TIME_set(notBefore, now)
+        CNIOBoringSSL_X509_set_notBefore(x, notBefore)
+        CNIOBoringSSL_ASN1_TIME_free(notBefore)
+        
+        now += 60 * 60 * 60  // Give ourselves an hour
+        let notAfter = CNIOBoringSSL_ASN1_TIME_new()!
+        CNIOBoringSSL_ASN1_TIME_set(notAfter, now)
+        CNIOBoringSSL_X509_set_notAfter(x, notAfter)
+        CNIOBoringSSL_ASN1_TIME_free(notAfter)
+        
+        CNIOBoringSSL_X509_set_pubkey(x, pkey)
+        
+//        CNIOBoringSSL_PEM_read_bio_X509(cert, nil, nil, nil)
+        
+        CNIOBoringSSL_X509_set_issuer_name(x, CNIOBoringSSL_X509_get_subject_name(_ref()!))
+        CNIOBoringSSL_X509_set_subject_name(x, subject)
+        CNIOBoringSSL_X509_NAME_free(subject)
+        addExtension(x509: x, nid: NID_basic_constraints, value: "critical,CA:FALSE")
+        addExtension(x509: x, nid: NID_subject_key_identifier, value: "hash")
+        addExtension(x509: x, nid: NID_subject_alt_name, value: "DNS:\(host)")
+        addExtension(x509: x, nid: NID_ext_key_usage, value: "serverAuth,OCSPSigning")
+        
+        CNIOBoringSSL_X509_sign(x, pkey, CNIOBoringSSL_EVP_sha256())
+        
+        // 将证书转换为 DER 格式的字节数组
+        var derBytesPointer: UnsafeMutablePointer<UInt8>? = nil
+        let derLength = CNIOBoringSSL_i2d_X509(x, &derBytesPointer)
+        
+        // 确保内存安全，创建 Data 对象，并释放分配的内存
+        let certData = Data(bytes: derBytesPointer!, count: Int(derLength))
+        CNIOBoringSSL_OPENSSL_free(derBytesPointer)
 
-                defer {
-                    CNIOBoringSSL_BIO_free(bio)
-                }
+        let cert = try! NIOSSLCertificate(bytes: Array(certData), format: .der)
+        CNIOBoringSSL_X509_free(x)
+        
+        return cert
+    }
+    
+    private static func _ref() -> OpaquePointer? {
+        let ref = cert.withUnsafeBytes { (ptr) -> OpaquePointer? in
+            let bio = CNIOBoringSSL_BIO_new_mem_buf(ptr.baseAddress, ptr.count)!
 
-                return CNIOBoringSSL_PEM_read_bio_X509(bio, nil, nil, nil)
+            defer {
+                CNIOBoringSSL_BIO_free(bio)
             }
-            return ref
+
+            return CNIOBoringSSL_PEM_read_bio_X509(bio, nil, nil, nil)
         }
+        return ref
+    }
     
     private static func randomSerialNumber() -> ASN1_INTEGER {
         let bytesToRead = 20
