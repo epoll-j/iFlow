b33c319959482c55ec52e398b86cd2116066c0c7
Dubhe <epoll@foxmail.com>
2024-07-31 18:10:49 +0800

    feat: 历史记录页面添加数据绑定

--- debugger/Pages/HistoryPage.swift
+++ debugger/Pages/HistoryPage.swift
@@ -6,30 +6,31 @@
 //
 
 import SwiftUI
+import ProxyService
+import RealmSwift
 
 struct HistoryPage: View {
-    let items = ["1", "2", "3"]
+    @ObservedResults(TaskModel.self) var tasks
     
     var body: some View {
-        
         List {
-            ForEach(items, id: \.self) { item in
+            ForEach(tasks, id: \.self) { item in
                 NavigationLink {
                     ItemListPage()
-                        .navigationBarTitle("#1", displayMode: .large)
+                        .navigationBarTitle("123", displayMode: .large)
                 } label: {
                     VStack(spacing: 20) {
                         HStack() {
-                            Text("#1").font(.body).bold()
+                            Text("#\(item.id.prefix(4))").font(.body).bold()
                             Spacer()
-                            Text("2022-02-03 23:22:11")
+                            Text("\(item.startTime.formattedDate())")
                             //                                Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
                         }
                         .frame(maxWidth: .infinity)
                         HStack() {
-                            Text("666个请求").font(.body).foregroundStyle(.gray)
+                            Text("\(item.sessionsCount)个请求").font(.body).foregroundStyle(.gray)
                             Spacer()
-                            Text("持续时间: 11分钟").foregroundStyle(.gray)
+                            Text("持续时间: \(item.stopTime)").foregroundStyle(.gray)
                         }
                         .frame(maxWidth: .infinity)
                     }.frame(maxWidth: .infinity)
@@ -41,9 +42,7 @@ struct HistoryPage: View {
     
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
-            for index in offsets {
-                //                modelContext.delete(items[index])
-            }
+            $tasks.remove(atOffsets: offsets)
         }
     }
 }
